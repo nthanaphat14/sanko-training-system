42232253ff0a713c75fa12c7a7b4ec8d6216abf2
nthanaphat14 <nthanaphat.t@gmail.com>
2026-03-13 06:20:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///employee.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SESSION_COOKIE_HTTPONLY"] = True
 app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
-app.config["SESSION_COOKIE_SECURE"] = True
+app.config["SESSION_COOKIE_SECURE"] = False
 app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=30)
 
 print("DATABASE =", app.config["SQLALCHEMY_DATABASE_URI"])
