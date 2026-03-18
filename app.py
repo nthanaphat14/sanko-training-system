dc33cb113b41b7fcf3c94cc2250dabc2325fd6ca
nthanaphat14 <nthanaphat.t@gmail.com>
2026-03-18 14:24:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1175,7 +1175,10 @@ def employees_list():
             Employee.em_id.asc()
         )
 
-    employees = query.all()
+    employees = Employee.query.order_by(
+        Employee.status.desc(),   # Active มาก่อน
+        Employee.start_work.asc()
+    ).all(
 
     # ✅ Summary counts (นับจากฐานจริง ไม่ใช่จาก filter)
     total_active = Employee.query.filter(Employee.status == "Active").count()
